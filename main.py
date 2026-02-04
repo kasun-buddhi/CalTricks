daa43c21ec43ffc0e6c6e25a614c7b1022be704c
banuka-b <banukabogahalanda99@gmail.com>
2026-02-04 11:45:28 +0530

    WIP : added scrape_page.

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) 
 from data_scraper.CraftNest import CraftNest
 url     = "https://craftnest.net/collections/cliparts"
-site    = CraftNest(url)
-site.save_auth()
+site    = CraftNest()
+site.scrape_categories()
