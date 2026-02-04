bc7c0abd278a434734e2bc38726922cc9a97316e
banuka-b <banukabogahalanda99@gmail.com>
2026-02-04 09:04:46 +0530

    WIP : create main.py .

---
+++ main.py
@@ -0,0 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) 
+from data_scraper.CraftNest import CraftNest
+url     = "https://craftnest.net/collections/cliparts"
+site    = CraftNest(url)
+site.save_auth()
