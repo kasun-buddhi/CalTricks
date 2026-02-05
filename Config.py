39f07305ddfb9583c3807d3e512e63ecf3647bf8
banuka-b <banukabogahalanda99@gmail.com>
2026-02-05 20:20:59 +0530

    added asset_download_file_path

--- Config.py
+++ Config.py
@@ -1,7 +1,8 @@
 import os
 
-AUTH_FILE           = "playwright/.auth/state.json"
-USERNAME            =  os.getenv("CRAFTNEST_USERNAME")
-PASSWORD            =  os.getenv("CRAFTNEST_PASSWORD")
-LOGIN_URL           = "https://craftnest.net/account/login"
-CRAFTNEST_TIMEOUT   = 10000
+AUTH_FILE                = "playwright/.auth/state.json"
+USERNAME                 =  os.getenv("CRAFTNEST_USERNAME")
+PASSWORD                 =  os.getenv("CRAFTNEST_PASSWORD")
+LOGIN_URL                = "https://craftnest.net/account/login"
+CRAFTNEST_TIMEOUT        = 10000
+DOWNLOAD_ASSET_LOCATION  = "/home/banuka03/Downloads/assets/"
