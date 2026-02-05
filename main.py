93ba4905d26c9e56b1af45f636a1fbb37d9f7901
banuka-b <banukabogahalanda99@gmail.com>
2026-02-05 20:20:19 +0530

    WIP : added download_asset.

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from data_scraper.SubCategory import SubCategory
 from data_scraper.Asset import Asset
 
 
-craft = CraftNest()
-
-# All classes reuse the same browser + same page
+craft       = CraftNest()
 sub         = SubCategory(craft)
 asset       = Asset(craft)
-asset_links = asset.setup()
+#asset_links = asset.setup()
+asset.download_asset()
