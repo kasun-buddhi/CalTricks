13f3b31274ea5992c74c9a59fc0bfd2e651b7cdc
banuka-b <banukabogahalanda99@gmail.com>
2026-02-07 11:16:36 +0530

    WIP : added Asset class

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from data_scraper.CraftNest import CraftNest
-from data_scraper.SubCategory import SubCategory
 from data_scraper.Asset import Asset
 
-
-craft       = CraftNest()
-sub         = SubCategory(craft)
-asset       = Asset(craft)
-#asset_links = asset.setup()
+craft = CraftNest()
+asset = Asset(craft)
 asset.download_asset()
