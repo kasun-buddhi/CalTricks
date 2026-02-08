da2094a75a3d8e91ac7a02ae48ab580b0c07c383
banuka-b <banukabogahalanda99@gmail.com>
2026-02-08 11:42:17 +0530

    WIP : added drive_uploader

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from data_scraper.CraftNest import CraftNest
 from data_scraper.Asset import Asset
+from drive.DriveUploader import DriveUploader
 
 craft = CraftNest()
 asset = Asset(craft)
-asset.download_asset()
+#asset.download_asset()
+
+uploader = DriveUploader()
+uploader.upload_asset_folder()
