0eb96f6ce40df1d769a519bd5ce2b5c6a281743d
banuka-b <banukabogahalanda99@gmail.com>
2026-02-08 11:40:44 +0530

    WIP : added token_oath

--- Config.py
+++ Config.py
@@ -1,8 +1,11 @@
 import os
 
-AUTH_FILE                = "playwright/.auth/state.json"
-USERNAME                 =  os.getenv("CRAFTNEST_USERNAME")
-PASSWORD                 =  os.getenv("CRAFTNEST_PASSWORD")
-LOGIN_URL                = "https://craftnest.net/account/login"
-CRAFTNEST_TIMEOUT        = 100000
-DOWNLOAD_ASSET_LOCATION  = "./Asset"
+AUTH_FILE                           = "playwright/.auth/state.json"
+USERNAME                            =  os.getenv("CRAFTNEST_USERNAME")
+PASSWORD                            =  os.getenv("CRAFTNEST_PASSWORD")
+LOGIN_URL                           = "https://craftnest.net/account/login"
+CRAFTNEST_TIMEOUT                   = 100000
+DOWNLOAD_ASSET_LOCATION             = "./Asset"
+TOKEN_PATH                          = os.getenv("TOKEN_PATH")
+DRIVE_SCOPES                        = ['https://www.googleapis.com/auth/drive.file']
+GOOGLE_DRIVE_PARENT_FOLDER_ID       = "117xKnajtunTH0XX_s9dwClUy_KaptS5Q"
