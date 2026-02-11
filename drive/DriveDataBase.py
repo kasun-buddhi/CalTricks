6aad0b93c4f4b9a20a75ac6d6406913ad0b40725
banuka-b <banukabogahalanda99@gmail.com>
2026-02-11 09:39:43 +0530

    WIP : Adde http error handling.

--- drive/DriveDataBase.py
+++ drive/DriveDataBase.py
@@ -3,9 +3,11 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.http import MediaFileUpload
+from googleapiclient.errors import HttpError
 import os
 import shutil
 from Config import *
+
 
 class DriveDataBase:
     def __init__(self,folder):
@@ -39,97 +41,134 @@ class DriveDataBase:
     
     def _check_exists(self, name, parent_id, is_folder = False):
         """Check if a file or folder already exists in Google Drive"""
-        # Build the query
-        query = f"name='{name}' and '{parent_id}' in parents and trashed = false"
-        # Add MIME type filter for folders only
-        if is_folder:
-            query       +=  " and mimeType='application/vnd.google-apps.folder'"
-        results          =  self.service.files().list(
-            q            =  query,
-            fields       = 'files(id, name)',
-            pageSize     =  1).execute()
-        files            =  results.get('files', [])
-        if files:
-            return files[0].get('id')
-        return None
-    
+        try:
+            # Build the query
+            query = f"name='{name}' and '{parent_id}' in parents and trashed = false"
+            # Add MIME type filter for folders only  ##Multipurpose Internet Mail Extensions
+            if is_folder:
+                query       +=  " and mimeType='application/vnd.google-apps.folder'"
+            results          =  self.service.files().list(
+                q            =  query,
+                fields       = 'files(id, name)',
+                pageSize     =  1).execute()
+            files            =  results.get('files', [])
+            if files:
+                return files[0].get('id')
+            return None
+        except HttpError as e :
+            print(f"HTTP Error : {name} - {e} ")
+        except Exception as e :
+            print(f"Error checking files: {e}")
+        
     def _create_folder(self, folder_name, parent_id):
         """Create a folder in Google Drive (or return existing)"""
-        # Check if folder already exists
-        existing_id     =  self._check_exists(folder_name, parent_id, is_folder=True)
-        if existing_id:
-            print(f"Folder already exists: {folder_name} (ID: {existing_id})")
-            return existing_id
-        file_metadata    = {
-            'name'       : folder_name,
-            'mimeType'   : 'application/vnd.google-apps.folder',
-            'parents'    : [parent_id]}
-        folder           = self.service.files().create(
-            body         = file_metadata,
-            fields       = 'id, name').execute()
-        print(f"Created folder: {folder_name} (ID: {folder.get('id')})")
-        return folder.get('id'),folder_name
+        try:
+            # Check if folder already exists
+            existing_id      =  self._check_exists(folder_name, parent_id, is_folder=True)
+            if existing_id:
+                print(f"Folder already exists: {folder_name} (ID: {existing_id})")
+                return existing_id
+            file_metadata    = {
+            'name'          : folder_name,
+            'mimeType'      : 'application/vnd.google-apps.folder',
+            'parents'       : [parent_id]}
+            folder           = self.service.files().create(
+            body             = file_metadata,
+            fields           = 'id, name').execute()
+            print(f"Created folder: {folder_name} (ID: {folder.get('id')})")
+            return folder.get('id')
+        except HttpError as e :
+            print(f" HTTP Error Creating Folders : {folder_name} : {e}")
+        except Exception as e : 
+            print(f"Error Creating folder : {folder_name} : {e}")   
     
     def _upload_file(self, file_path, parent_id):
         """Upload a single file to Google Drive (skip if exists)"""
-        file_name        = os.path.basename(file_path)
-        # Check if file already exists
-        existing_id      = self._check_exists(file_name, parent_id, is_folder=False)
-        if existing_id:
-            print(f"File already exists, skipping: {file_name}")
+        try:
+            file_name        = os.path.basename(file_path)
+            # Check if file already exists
+            existing_id      = self._check_exists(file_name, parent_id, is_folder=False)
+            if existing_id:
+                print(f"File already exists, skipping: {file_name}")
+                return None
+            file_metadata   = {
+            'name'          : file_name,
+            'parents'       : [parent_id]}
+            media           = MediaFileUpload(file_path, resumable=True)
+            print(f"Uploading: {file_name}...")
+            uploaded_file   = self.service.files().create(
+            body            = file_metadata,
+            media_body      = media,
+            fields          = 'id, name, webViewLink').execute()
+            filename        = uploaded_file.get('name')
+            print(f"Uploaded: {filename}")
+            return uploaded_file, filename
+        except HttpError as e :
+            if e.resp.status == 403 :
+                if "storageQuotaExceeded" in str(e):
+                    print(f"Storage Almost full")
+                    print(f"Cannot upload : {file_name}")
+                    raise Exception("Storage quota exceeded - upload stopped") from e
+                else:
+                    print(f"Permission error uploading : {file_name} : {e}")
+            elif e.resp.status == 404 :
+                print(f"file not found : {file_name: {e}}")
+            elif e.resp.status == 400 :
+                print(f" Invalid request for {file_name} : {e}")
+            else : 
+                print(f"HTTP Error {e.resp.status}  uploading : {file_name} : {e}")
             return None
-        file_metadata   = {
-            'name'      : file_name,
-            'parents'   : [parent_id]}
-        media           = MediaFileUpload(file_path, resumable=True)
-        print(f"Uploading: {file_name}...")
-        uploaded_file   = self.service.files().create(
-            body        = file_metadata,
-            media_body  = media,
-            fields      = 'id, name, webViewLink').execute()
-        filename = uploaded_file.get('name')
-        print(f"Uploaded: {filename}")
-        return uploaded_file, filename
+        except Exception as e :
+            print(f" Unexpected error uploading : {file_name} : {e}")
+            return None
     
     def _upload_folder(self, local_path, parent_id):
         """Recursively upload folder structure to Google Drive"""
-        # Get all items in the current directory
-        items         = os.listdir(local_path)
-        for item in items:
-            item_path = os.path.join(local_path, item)
-            if os.path.isdir(item_path):
-                # It's a folder - create it in Drive and recurse
-                print(f"Processing folder: {item}")
-                folder_id = self._create_folder(item, parent_id)
-                # Recursively upload contents of this folder ( sub foders)
-                self._upload_folder(item_path, folder_id)
-            elif os.path.isfile(item_path):
-                # It's a file - upload it
-                files = self._upload_file(item_path, parent_id)
-        return item
-
+        try:
+           # Get all items in the current directory
+            items           = os.listdir(local_path)
+            for item in items:
+                item_path   = os.path.join(local_path, item)
+                if os.path.isdir(item_path):
+                    # It's a folder - create it in Drive and recurse
+                    print(f"Processing folder: {item}")
+                    folder_id = self._create_folder(item, parent_id)
+                    # Recursively upload contents of this folder ( sub foders)
+                    self._upload_folder(item_path, folder_id)
+                elif os.path.isfile(item_path):
+                    # It's a file - upload it
+                    files = self._upload_file(item_path, parent_id)
+            return item
+        except  Exception as e :
+            print(f" Error in Folder upload : {e}")
+            
     def upload_asset_folder(self):
         """Upload entire Asset folder structure to Google Drive"""
-        creds            = self._authenticate()
-        self.service     = build("drive", "v3", credentials=creds)
-        print(f"Starting upload of: {self.folder}")
-        if not os.path.exists(self.folder):
-            print(f"Error: {self.folder} does not exist!")
-            return
-        # Create main "Asset" folder in Google Drive (or use existing)
-        main_folder_name        = os.path.basename(self.folder)
-        main_folder_id         = self._create_folder(main_folder_name, GOOGLE_DRIVE_PARENT_FOLDER_ID)
-        # Upload all contents recursively
-        filename = self._upload_folder(self.folder, main_folder_id)
-        print("Upload completed successfully!")
-        return filename      
+        try:
+            creds            = self._authenticate()
+            self.service     = build("drive", "v3", credentials=creds)
+            print(f"Starting upload of: {self.folder}")
+            if not os.path.exists(self.folder):
+                print(f"Error: {self.folder} does not exist!")
+                return
+            # Create main "Asset" folder in Google Drive (or use existing)
+            main_folder_name        = os.path.basename(self.folder)
+            main_folder_id         = self._create_folder(main_folder_name, GOOGLE_DRIVE_PARENT_FOLDER_ID)
+            # Upload all contents recursively
+            filename = self._upload_folder(self.folder, main_folder_id)
+            print("Upload completed successfully!")
+            return filename 
+        except Exception as e :
+            print(f"upload failed : {e}")
+            return None
         
-    
     def delete_local_folder(self,folder_path):
         """ deleted the local sub category folder after upload in drive"""
         try:
+            deleted_folders = []
             if os.path.exists(folder_path):
                 shutil.rmtree(folder_path)
+                deleted_folders.append(folder_path)
                 print(f"deleted local sub category folder : {folder_path}")
                 return True
             else:
