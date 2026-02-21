712b777b71a0598f921c17ccc1261b8bdeaf0755
banuka-b <banukabogahalanda99@gmail.com>
2026-02-21 08:37:54 +0530

    WIP : added browser_restart.

--- Config.py
+++ Config.py
@@ -9,3 +9,4 @@ DOWNLOAD_ASSET_LOCATION             = "./Asset"
 TOKEN_PATH                          = os.getenv("TOKEN_PATH")
 DRIVE_SCOPES                        = ['https://www.googleapis.com/auth/drive.file']
 GOOGLE_DRIVE_PARENT_FOLDER_ID       = "117xKnajtunTH0XX_s9dwClUy_KaptS5Q"
+BROWSER_RESTART_EVERY               = 10
