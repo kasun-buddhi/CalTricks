3e275894928ec7444824786b9509bf52f1b2710b
banuka-b <banukabogahalanda99@gmail.com>
2026-03-09 21:20:29 +0530

    WIP : Added asset_download.

--- Config.py
+++ Config.py
@@ -10,3 +10,5 @@ TOKEN_PATH                          = os.getenv("TOKEN_PATH")
 DRIVE_SCOPES                        = ['https://www.googleapis.com/auth/drive.file']
 GOOGLE_DRIVE_PARENT_FOLDER_ID       = "117xKnajtunTH0XX_s9dwClUy_KaptS5Q"
 BROWSER_RESTART_EVERY               = 10
+#DOWNLOAD_ASSET                     = True   # download zip + scrape data + upload
+DOWNLOAD_ASSET                      = False  # scrape data only (thumbnail, images, JSON) + upload
